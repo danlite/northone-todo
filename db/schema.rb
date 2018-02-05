b0ca58e5fcde21be8c25ea71de93daa2002fb5e8
Daniel Lichty <djlichty@gmail.com>
2018-02-05 18:18:09 -0500

    change order of migrations
    (yes, this is horrible. don't do this in the real world)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205203351) do
+ActiveRecord::Schema.define(version: 20180205203352) do
 
   create_table "task_lists", force: :cascade do |t|
     t.string "name"
