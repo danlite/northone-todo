a36fa460aadef6619b86b8353efc484bb40f623d
Daniel Lichty <djlichty@gmail.com>
2018-02-07 10:22:04 -0500

    add error responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,38 @@
 class ApplicationController < ActionController::API
+  rescue_from Exception, with: :render_error
+
+  def routing_catch_all
+    raise ActionController::RoutingError.new("Route not found: `/#{params[:path]}`")
+  end
+
   protected
 
-  def render_result(result = { success: true })
-    render json: { result: result }
+  def render_error(error)
+    message = error.message
+
+    case error
+    when ActionController::RoutingError, ActiveRecord::RecordNotFound
+      code = 404
+    when ActiveRecord::ActiveRecordError, ActionController::ParameterMissing
+      code = 400
+    else
+      code = 500
+      message = 'Something went wrong.'
+    end
+
+    logger.debug "\n#{error.class}: #{error.message}\n"
+
+    render json: {
+      error: {
+        message: message,
+        code: code
+      }
+    }
+  end
+
+  def render_result(result = {})
+    render json: {
+      result: result
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :task_lists do
     resources :tasks
   end
+
+  match '*path', to: 'application#routing_catch_all', via: :all
 end
